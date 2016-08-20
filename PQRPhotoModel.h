15bb2b5fa67527fbe390a5f24c3c057a331da1ed
Nathan Fennel <nathanfennel@gmail.com>
2016-08-19 21:52:55 -0400

    Created Photo Model

---
+++ PQRPhotoModel.h
@@ -0,0 +1,13 @@
+//
+//  PQRPhotoModel.h
+//  Photo ReQuester
+//
+//  Created by Nathan Fennel on 8/19/16.
+//  Copyright © 2016 Nathan Fennel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PQRPhotoModel : NSObject
+
+@end
---
+++ PQRPhotoModel.m
@@ -0,0 +1,13 @@
+//
+//  PQRPhotoModel.m
+//  Photo ReQuester
+//
+//  Created by Nathan Fennel on 8/19/16.
+//  Copyright © 2016 Nathan Fennel. All rights reserved.
+//
+
+#import "PQRPhotoModel.h"
+
+@implementation PQRPhotoModel
+
+@end
