916dfda4041d80b2ffb6f1420806213fc389e154
Nathan Fennel <nathanfennel@gmail.com>
2016-08-19 23:57:15 -0400

    Created User Manager

---
+++ Photo ReQuester/PQRUserManager.m
@@ -0,0 +1,13 @@
+//
+//  PQRUserManager.m
+//  Photo ReQuester
+//
+//  Created by Nathan Fennel on 8/19/16.
+//  Copyright © 2016 Nathan Fennel. All rights reserved.
+//
+
+#import "PQRUserManager.h"
+
+@implementation PQRUserManager
+
+@end
