fd2f21be53613718ff693af030d26da013660337
Nathan Fennel <nathanfennel@gmail.com>
2016-08-19 21:54:11 -0400

    Create Seller Data Manager

---
+++ PQRSellerDataManager.h
@@ -0,0 +1,13 @@
+//
+//  PQRSellerDataManager.h
+//  Photo ReQuester
+//
+//  Created by Nathan Fennel on 8/19/16.
+//  Copyright © 2016 Nathan Fennel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PQRSellerDataManager : NSObject
+
+@end
---
+++ PQRSellerDataManager.m
@@ -0,0 +1,13 @@
+//
+//  PQRSellerDataManager.m
+//  Photo ReQuester
+//
+//  Created by Nathan Fennel on 8/19/16.
+//  Copyright © 2016 Nathan Fennel. All rights reserved.
+//
+
+#import "PQRSellerDataManager.h"
+
+@implementation PQRSellerDataManager
+
+@end
